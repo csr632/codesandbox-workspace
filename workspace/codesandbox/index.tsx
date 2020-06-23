4e431c298db8de38777dcfe2f1cef14f7740b138
sirui.csr <sirui.csr@alibaba-inc.com>
2020-06-24 02:16:09 +0800

    fix csb

--- workspace/codesandbox/index.tsx
+++ workspace/codesandbox/index.tsx
@@ -3,4 +3,10 @@ import ReactDom from "react-dom";
 
 import Demo from "../demos/demo2/index$";
 
-ReactDom.render(<Demo />, document.querySelector("#root"));
+if (!document.querySelector("#app-root")) {
+  const root = document.createElement("div");
+  root.id = "app-root";
+  document.body.appendChild(root);
+}
+
+ReactDom.render(<Demo />, document.querySelector("#app-root"));
