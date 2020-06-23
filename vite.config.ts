b463280da9925bd4029f5d0baba823ffa5d3fcde
sirui.csr <sirui.csr@alibaba-inc.com>
2020-06-24 02:05:25 +0800

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -4,13 +4,13 @@ import pages from "vite-plugin-react-pages";
 import mdx from "vite-plugin-mdx";
 import * as path from "path";
 
-
 module.exports = {
   jsx: "react",
   plugins: [vpr, mdx(), pages(path.join(__dirname, "./workspace/demos"))],
   root: path.join(__dirname, "./workspace"),
   optimizeDeps: {
     include: ["styled-components", "@mdx-js/react", "react-router-dom"],
+    exclude: ["@types/react", "@types/react-dom", "react-dom", "react"],
   },
   minify: false,
 } as UserConfig;
